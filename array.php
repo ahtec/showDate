3068a275424087615394237d0d8de595f3571293
ahtec <gerarddoets@gmail.com>
2017-10-24 14:32:20 +0200

    3e first commit

--- array.php
+++ array.php
@@ -1,6 +1,6 @@
 <?php
 
-$nummers = array("zondag" ,"maandag","dinsdg");
+$nummers = array("zondag" ,"maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");
 //echo $nummers[5];
 $lenghtArray = count($nummers);
 
@@ -9,9 +9,6 @@ for ($i=0;$i<$lenghtArray;$i++) {
 }
 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* e in the editor.
  */
 
